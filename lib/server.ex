6726c0fdf66f936b37a93f1794365139f836f325
Omar Bassem <omarbassem889@gmail.com>
2026-08-07 12:16:57 +0300

    Public facing API for user connection & handle_cast  msg sending implementation

--- lib/server.ex
+++ lib/server.ex
@@ -12,4 +12,13 @@ defmodule PotionChat.Server do
     {:noreply, new_state}
   end
 
+  @impl true
+  def handle_cast({:new_msg, sender_pid, msg}, state) do
+    Enum.each(state, fn (user_pid) -> send(user_pid, "#{inspect(sender_pid)}: #{msg}") end)
+    {:noreply, state}
+  end
+
+  def connect(server_pid) do
+    GenServer.cast(server_pid, {:connect_user, self()})
+  end
 end
